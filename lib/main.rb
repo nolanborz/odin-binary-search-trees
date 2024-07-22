0630c7702353ec36983f1fd1c3854769b57c6f83
nolanborz <nolanrborzoni@gmail.com>
2024-07-22 11:43:28 +0900

    Add insert and delete node functions
    Added an insert function which inserts a node
    into the tree, and added a delete function which
    deletes a node and reorganizes the tree.

--- lib/main.rb
+++ lib/main.rb
@@ -26,9 +26,66 @@ class Tree
     node.right = build_tree(sorted_arr, mid + 1, last)
     return node
   end
+
+  def pretty_print(node = @root, prefix = '', is_left = true)
+    pretty_print(node.right, "#{prefix}#{is_left ? '│   ' : '    '}", false) if node.right
+    puts "#{prefix}#{is_left ? '└── ' : '┌── '}#{node.value}"
+    pretty_print(node.left, "#{prefix}#{is_left ? '    ' : '│   '}", true) if node.left
+  end
+
+  def insert(value)
+    @root = insert_recursive(@root, value)
+  end
+  def delete(value)
+    @root = delete_node(@root, value)
+  end
+  private
+  def delete_node(root, value)
+    return root if root.nil?
+    
+    if value < root.value
+      root.left = delete_node(root.left, value)
+    elsif value > root.value
+      root.right = delete_node(root.right, value)
+    else
+      #if root is same as value to be deleted, we've found what we need to delete
+      return root.right if root.left.nil?
+      return root.left if root.right.nil?
+      #node with 2 children
+      root.value = min_value(root.right)
+      root.right = delete_node(root.right, root.value)
+    end
+    root
+  end
+  def min_value(node)
+    min_v = node.value
+    while node.left
+      min_v = node.left.value
+      node = node.left
+    end
+    min_v
+  end
+  def insert_recursive(node, value)
+    return Node.new(value) if node.nil?
+  
+    if value < node.value
+      node.left = insert_recursive(node.left, value)
+    elsif value > node.value
+      node.right = insert_recursive(node.right, value)
+    else
+      # Value already exists in the tree, do nothing
+      return node
+    end
+    node
+  end
+
 end
 
 array = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324]
 great_eternal_tree = Tree.new(array)
-puts great_eternal_tree.root
+puts great_eternal_tree.root.value
+great_eternal_tree.insert(12)
+great_eternal_tree.pretty_print
+great_eternal_tree.delete(67)
+great_eternal_tree.pretty_print
   
